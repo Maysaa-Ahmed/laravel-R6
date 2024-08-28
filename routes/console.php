21e46d4cb30a062e55de56599798a3a4476804ec
maysaa <maysaa.ahmed94@gmail.com>
2024-08-28 11:42:48 -0700

    task15

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+schedule::command('backup:database')->daily(); // backup db
+
+
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
